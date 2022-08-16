62d1697c5145918648a333c4c090d9fc114e2730
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-08-16 02:50:19 +0100

    added a footer

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,18 @@
 <script>
   import "./app.css"
   import Header from "./lib/component/Header.svelte"
+  import Button from "./lib/component/Button.svelte";
+  import Footer from "./lib/component/Footer.svelte";
 
 </script>
 
 <main>
 
 <Header/>
+<main class="">
+  <Button content="here"/>
+</main>
+<Footer/>
 
 </main>
 
---
+++ src/lib/component/Button.svelte
@@ -0,0 +1,11 @@
+<script>
+export let content;
+</script>
+
+<button class="px-3 py-1 text-slate-800 dark:text-slate-200 bg-gray-100 opacity-60 dark:bg-gray-600 dark:bg-opacity-60 hover:bg-opacity-95 dark:hover:bg-opacity-95">
+    {content}
+</button>
+
+<style>
+
+</style>
