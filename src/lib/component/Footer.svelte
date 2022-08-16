35af76bda1f7f76694edd1bbfd8f27c5b9fec04a
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-08-16 14:43:59 +0100

    added footer

--- src/lib/component/Footer.svelte
+++ src/lib/component/Footer.svelte
@@ -2,11 +2,15 @@
 
 </script>
 
-<div class="w-full flex justify-center bg-slate-400 dark:bg-slate-800 py-1 px-2 border-b border-slate-800 dark:border-slate-200 fixed bottom-0">
-    <div class="flex gap-2 items-center">
+<div class="w-full flex flex-col md:flex-row justify-around items-center bg-slate-400 dark:bg-slate-800 py-5 px-10 border-t border-slate-800 dark:border-slate-200 fixed bottom-0">
+    <div class="flex flex-col gap-2 items-center">
         <!-- logo and text -->
-        <svg class="w-10 h-10 stroke-slate-800 dark:stroke-slate-200" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"></path></svg>        <div class="text-slate-800 dark:text-slate-200 font-bold select-none">TODO app with svelte</div>
+        <svg class="w-16 h-16 stroke-slate-800 dark:stroke-slate-200" fill="none" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-6 9l2 2 4-4"></path></svg>        <div class="text-slate-800 dark:text-slate-200 font-bold select-none">TODO app with svelte</div>
+        <div class="text-slate-800 dark:text-slate-200 text-sm font-normal select-none text-center capitalize">bleeding edge, blazingly fast, next generation TODO app</div>
     </div>
+    <span class="text-slate-800 dark:text-slate-200 font-bold text-2xl text-center my-3">
+        made with <img class="inline" src="./src/assets/svelte.svg" alt="svelte logo"> by <a href="https://www.linkedin.com/in/chakib-ouzane/" target=”_blank” class="underline">ch33kaboo</a>
+    </span>
 </div>
 
 <style></style>
