5c96470a0bffbcdb8166f3cdec5d9683973c612c
ch33kaboo <mc.ouzane@esi-sba.dz>
2022-08-18 17:03:40 +0100

    added clear list action

--- src/App.svelte
+++ src/App.svelte
@@ -3,29 +3,7 @@
   import Header from "./lib/component/Header.svelte"
   import Button from "./lib/component/Button.svelte";
   import Footer from "./lib/component/Footer.svelte";
-  import TodoItem from "./lib/component/TodoItem.svelte";
   import TodoList from "./lib/component/TodoList.svelte";
-
-  let todos = [
-    {
-      title : "go to the gym",
-      time : "8:30am"
-    },
-    {
-      title : "hangout with friends",
-      description : "got to meet Ali and Sophia at the park",
-      time : "9:45am"
-    },
-    {
-      title : "have lunch",
-      description : "going to a mexican restaurant",
-      time : "11:30am"
-    },
-    {
-      title : "have a nap",
-      time : "1:00pm"
-    },
-  ]
 
   let dark = ""
 
@@ -45,14 +23,7 @@
       </main>
     </main>
 
-    <TodoList>
-      <slot>
-        {#each todos as todo}
-          <TodoItem title="{todo.title}" description="{todo.description}" time="{todo.time}"/>
-        {/each}
-      </slot>
-    </TodoList>
-
+    <TodoList/>
     <Footer/>
 
   </div>
--- src/lib/component/TodoList.svelte
+++ src/lib/component/TodoList.svelte
@@ -1,15 +1,44 @@
 <script>
     import Button from "./Button.svelte";
+    import TodoItem from "./TodoItem.svelte"
+
+
+    let todos = [
+    {
+      title : "go to the gym",
+      time : "8:30am"
+    },
+    {
+      title : "hangout with friends",
+      description : "got to meet Ali and Sophia at the park",
+      time : "9:45am"
+    },
+    {
+      title : "have lunch",
+      description : "going to a mexican restaurant",
+      time : "11:30am"
+    },
+    {
+      title : "have a nap",
+      time : "1:00pm"
+    },
+  ]
+
+const clearTasks = () => {
+    todos = []
+}
 
 </script>
 
 <div class="mx-20 mb-9 mt-11 px-10 py-4 border border-slate-800 dark:border-slate-200 border-opacity-60 dark:border-opacity-60 rounded-md bg-slate-400 dark:bg-slate-800 bg-opacity-40 dark:bg-opacity-40 flex flex-col gap-1">
-    <slot></slot>
+    {#each todos as todo}
+        <TodoItem title="{todo.title}" description="{todo.description}" time="{todo.time}"/>
+    {/each}
     <div class="flex justify-end items-center gap-3">
         <div class="rounded-md overflow-hidden mt-3">
             <Button content="add a task"/>
         </div>
-        <div class="rounded-md overflow-hidden mt-3">
+        <div on:click="{clearTasks}" class="rounded-md overflow-hidden mt-3">
             <Button content="clear all tasks"/>
         </div>
     </div>
